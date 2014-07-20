c87d767b950c6019a47999128a3653e2c503cc17
mythbusterma <wiirocksme@gmail.com>
2014-07-20 17:17:10 -0400

    Changed some method signatures, added some database methods

--- src/com/github/rolecraftdev/data/PlayerData.java
+++ src/com/github/rolecraftdev/data/PlayerData.java
@@ -4,12 +4,24 @@ import java.util.UUID;
 
 public final class PlayerData {
 	private final UUID playerId;
+	private String name;
 
-	public PlayerData(final UUID playerId) {
+	public PlayerData(final UUID playerId, final String name) {
 		this.playerId = playerId;
+		this.name = name;
 	}
 
 	public UUID getPlayerId() {
 		return playerId;
+	}
+	
+	/**
+	 * Called when loaded in SQL
+	 * @deprecated
+	 * DO NOT CALL
+	 */
+	@Deprecated
+	public void intialize () {
+		
 	}
 }
--- src/com/github/rolecraftdev/data/storage/DataStore.java
+++ src/com/github/rolecraftdev/data/storage/DataStore.java
@@ -1,5 +1,16 @@
 package com.github.rolecraftdev.data.storage;
 
+import com.github.rolecraftdev.data.PlayerData;
+
 public abstract class DataStore {
+	
+	public static final String pt = "playertable";
+	public static final String mdt = "metadatatable";
+	
+	public abstract void intialise();
+	
+	public abstract void requestPlayerData(final PlayerData callback);
+	
+	public abstract void commitPlayerData(final PlayerData commit);
 
 }
