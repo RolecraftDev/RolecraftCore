9e1cad7d490067687039427ca786ec4c2a35f25e
mythbusterma <wiirocksme@gmail.com>
2014-07-20 17:26:28 -0400

    Some database logic

--- src/com/github/rolecraftdev/data/storage/DataStore.java
+++ src/com/github/rolecraftdev/data/storage/DataStore.java
@@ -1,5 +1,10 @@
 package com.github.rolecraftdev.data.storage;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.github.rolecraftdev.data.PlayerData;
 
 public abstract class DataStore {
@@ -12,5 +17,18 @@ public abstract class DataStore {
 	public abstract void requestPlayerData(final PlayerData callback);
 	
 	public abstract void commitPlayerData(final PlayerData commit);
+	
+	protected abstract Connection getConnection();
+	
+	protected void close (PreparedStatement ps, ResultSet rs) {
+		try {
+			if(ps != null)
+				ps.close();
+			if(rs != null) 
+				rs.close();
+		} catch (SQLException e) {
+			// swallow exception
+		}
+	}
 
 }
--- src/com/github/rolecraftdev/data/storage/MySQLDataStore.java
+++ src/com/github/rolecraftdev/data/storage/MySQLDataStore.java
@@ -1,6 +1,30 @@
 package com.github.rolecraftdev.data.storage;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.github.rolecraftdev.data.PlayerData;
+
 
 public final class MySQLDataStore extends DataStore {
+
+	@Override
+	public void intialise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void requestPlayerData(PlayerData callback) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void commitPlayerData(PlayerData commit) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
