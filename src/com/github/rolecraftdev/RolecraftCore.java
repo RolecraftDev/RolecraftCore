6f48199467fa48a1662ee2da3233e619a37f6106
DziNeIT <o.dzineit@gmail.com>
2014-07-20 18:59:24 +0100

    Plugin class

---
+++ src/com/github/rolecraftdev/RolecraftCore.java
@@ -0,0 +1,13 @@
+package com.github.rolecraftdev;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RolecraftCore extends JavaPlugin {
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
