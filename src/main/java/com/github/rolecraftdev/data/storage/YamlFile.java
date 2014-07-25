5c949c9262b5e0a8a28cde94b4cc9ee59e262d01
DziNeIT <o.dzineit@gmail.com>
2014-07-25 18:51:53 +0100

    YamlFile license header

--- src/main/java/com/github/rolecraftdev/data/storage/YamlFile.java
+++ src/main/java/com/github/rolecraftdev/data/storage/YamlFile.java
@@ -1,14 +1,39 @@
+/*
+ * This file is part of RolecraftCore.
+ *
+ * Copyright (c) 2014 RolecraftDev <http://rolecraftdev.github.com>
+ * RolecraftCore is licensed under the Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License. To view a copy of this
+ * license, visit http://creativecommons.org/licenses/by-nc-nd/3.0
+ *
+ * As long as you follow the following terms, you are free to copy and redistribute
+ * the material in any medium or format.
+ *
+ * You must give appropriate credit, provide a link to the license, and indicate
+ * whether any changes were made to the material. You may do so in any reasonable
+ * manner, but not in any way which suggests the licensor endorses you or your use.
+ *
+ * You may not use the material for commercial purposes.
+ *
+ * If you remix, transform, or build upon the material, you may not distribute the
+ * modified material.
+ *
+ * You may not apply legal terms or technological measures that legally restrict
+ * others from doing anything the license permits.
+ *
+ * DISCLAIMER: This is a human-readable summary of (and not a substitute for) the
+ * license.
+ */
 package com.github.rolecraftdev.data.storage;
-
-import java.io.File;
-import java.io.IOException;
 
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
-public class YamlFile extends YamlConfiguration {
+import java.io.File;
+import java.io.IOException;
 
+public class YamlFile extends YamlConfiguration {
     private final File file;
 
     public YamlFile(final Plugin plugin, final String fileName,
@@ -32,11 +57,9 @@ public class YamlFile extends YamlConfiguration {
     public void reload() {
         try {
             load(getFile());
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        catch(InvalidConfigurationException e) {
+        } catch (InvalidConfigurationException e) {
             e.printStackTrace();
         }
     }
@@ -44,8 +67,7 @@ public class YamlFile extends YamlConfiguration {
     public void save() {
         try {
             save(getFile());
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
