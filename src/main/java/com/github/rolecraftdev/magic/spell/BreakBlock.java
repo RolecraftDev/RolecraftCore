542fb088ea89c3ec56dc2151aff87deac81f7ad6
DziNeIT <o.dzineit@gmail.com>
2014-08-07 21:07:47 +0100

    Add Break Block recipe

--- src/main/java/com/github/rolecraftdev/magic/spell/BreakBlock.java
+++ src/main/java/com/github/rolecraftdev/magic/spell/BreakBlock.java
@@ -1,12 +1,17 @@
 package com.github.rolecraftdev.magic.spell;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class BreakBlock implements Spell {
     public BreakBlock(SpellManager spellManager) {
@@ -25,7 +30,7 @@ public class BreakBlock implements Spell {
 
     @Override
     public float estimateLeftClickMana(Player ply, Block block, int modifier) {
-        return 4;
+        return 3;
     }
 
     @Override
@@ -47,7 +52,7 @@ public class BreakBlock implements Spell {
                 block.setType(Material.AIR);
             }
         }
-        return 4;
+        return 3;
     }
 
     @Override
@@ -57,7 +62,18 @@ public class BreakBlock implements Spell {
 
     @Override
     public Recipe getWandRecipe() {
-        return null;
+        // same for each
+        ItemStack result = new ItemStack(Material.STICK);
+        ItemMeta meta = result.getItemMeta();
+        meta.setDisplayName(ChatColor.AQUA + getName());
+        meta.addEnchant(Enchantment.LUCK, 10, true);
+        ShapedRecipe recipe = new ShapedRecipe(result);
+        // custom recipe stuff
+        recipe.shape("IPI", "PBP", "IPI");
+        recipe.setIngredient('I', Material.IRON_INGOT);
+        recipe.setIngredient('P', Material.DIAMOND_PICKAXE);
+        recipe.setIngredient('B', Material.IRON_BLOCK);
+        return recipe;
     }
 
 }
